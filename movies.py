dd3a73fc6e99bf227bc5419805b9ac3c3a8bce58
Simon Prunet <simon.prunet@oca.eu>
2021-12-09 12:18:40 +0100

    debug

--- movies.py
+++ movies.py
@@ -13,13 +13,13 @@ def create_movie(noisy, truth, mask, pred):
 	'''
 
 	nobs = noisy.shape[0]
-	fig, (ax1,ax2,ax3,ax4) = plt.subplots(2,2)
+	fig, axes = plt.subplots(2,2)
 	frames = []
 	for i in range(nobs):
-		pl1 = ax1.imshow(noisy[i,:,:],animated=True)
-		pl2 = ax2.imshow(truth[i,:,:],animated=True)
-		pl3 = ax3.imshow(mask[i,:,:] ,animated=True)
-		pl4 = ax4.imshow(pred[i,:,:], animated=True)
+		pl1 = axes[0,0].imshow(noisy[i,:,:],animated=True)
+		pl2 = axes[0,1].imshow(truth[i,:,:],animated=True)
+		pl3 = axes[1,0].imshow(mask[i,:,:] ,animated=True)
+		pl4 = axes[1,1].imshow(pred[i,:,:], animated=True)
 		frames.append([pl1,pl2,pl3,pl4])
 	
 	ani = animation.ArtistAnimation(fig,frames,interval=250,blit=True)
